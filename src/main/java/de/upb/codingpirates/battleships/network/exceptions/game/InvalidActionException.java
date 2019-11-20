71b8a07cb8312d816b3be7c4ff2ce2135521d15c
Paul Becker <paube@mail.upb.de>
2019-11-20 16:49:24 +0100

    Add Battleship exceptions and handling to network

--- src/main/java/de/upb/codingpirates/battleships/network/exceptions/game/InvalidActionException.java
+++ src/main/java/de/upb/codingpirates/battleships/network/exceptions/game/InvalidActionException.java
@@ -1,22 +1,9 @@
 package de.upb.codingpirates.battleships.network.exceptions.game;
 
 import de.upb.codingpirates.battleships.logic.util.ErrorType;
-import de.upb.codingpirates.battleships.network.id.Id;
-
-import javax.annotation.Nonnull;
 
 public class InvalidActionException extends GameException {
     public InvalidActionException(String message) {
         super(message, ErrorType.INVALID_ACTION);
-    }
-
-    public InvalidActionException(@Nonnull String message, @Nonnull Id connectionId) {
-        this(message);
-        this.setConnectionId(connectionId);
-    }
-
-    public InvalidActionException(@Nonnull String message, @Nonnull Id connectionId, int messageId) {
-        this(message, connectionId);
-        this.setMessageId(messageId);
     }
 }
--- src/main/java/de/upb/codingpirates/battleships/network/exceptions/game/NotAllowedException.java
+++ src/main/java/de/upb/codingpirates/battleships/network/exceptions/game/NotAllowedException.java
@@ -1,22 +1,9 @@
 package de.upb.codingpirates.battleships.network.exceptions.game;
 
 import de.upb.codingpirates.battleships.logic.util.ErrorType;
-import de.upb.codingpirates.battleships.network.id.Id;
-
-import javax.annotation.Nonnull;
 
 public class NotAllowedException extends GameException {
     public NotAllowedException(String message) {
         super(message, ErrorType.NOT_ALLOWED);
-    }
-
-    public NotAllowedException(@Nonnull String message, @Nonnull Id connectionId) {
-        this(message);
-        this.setConnectionId(connectionId);
-    }
-
-    public NotAllowedException(@Nonnull String message, @Nonnull Id connectionId, int messageId) {
-        this(message, connectionId);
-        this.setMessageId(messageId);
     }
 }
