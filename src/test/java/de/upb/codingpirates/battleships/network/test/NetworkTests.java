a04549506fce5d6d4e017f22aa61639cd4a5242c
Paul Becker <paube@mail.upb.de>
2020-01-22 19:37:23 +0100

    fix test guice module

--- src/test/java/de/upb/codingpirates/battleships/network/test/NetworkTests.java
+++ src/test/java/de/upb/codingpirates/battleships/network/test/NetworkTests.java
@@ -16,6 +16,7 @@ import de.upb.codingpirates.battleships.network.message.Parser;
 import de.upb.codingpirates.battleships.network.network.module.ClientNetworkModule;
 import de.upb.codingpirates.battleships.network.network.module.ServerNetworkModule;
 import de.upb.codingpirates.battleships.network.util.ClientReaderMethod;
+import de.upb.codingpirates.battleships.network.util.DefaultReaderMethod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
@@ -118,7 +119,7 @@ public class NetworkTests {
 
             this.bind(ConnectionHandler.class).to(ClientConnector.class).in(Singleton.class);
             this.bind(de.upb.codingpirates.battleships.client.handler.TestMessageHandler.class);
-            this.bind(ClientReaderMethod.class).to(ClientReaderMethod.class);
+            this.bind(ClientReaderMethod.class).to(DefaultReaderMethod.class);
         }
     }
 
